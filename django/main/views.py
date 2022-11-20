568446178cddfa8dcb9bc742a21dfed89a63744c
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2022-11-20 11:30:53 +0300

    finish the main page and 4 sections from the tutorial

---
+++ django/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# create a view that responsible to show themain page that will appear when any user will navigate to the website
+
+def main_view(request):
+    """
+    landing page when user navigates into the website
+    that will return a http response
+    """
+    template_path = "main/main.html"
+    return render(request, template_path, {"name": "automax"})
+    # {"name": "automax"} this dictionary is for pass avalue to the template
