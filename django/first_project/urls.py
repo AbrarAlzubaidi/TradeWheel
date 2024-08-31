232479d3700c60030aee86e3254a8665bffc1617
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2024-09-01 02:02:59 +0300

    make changes for deployment

--- django/first_project/urls.py
+++ django/first_project/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
 #   - the second one is the folder that will point to
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
